b36de26d94effdc1a46aec5594a751ad39eacb39
Nicolas Valencia <n_valencia@hotmail.es>
2022-08-24 05:08:44 -0500

    Loading page working for http and https URLs

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-# This is a sample Python script.
+import time
+import pandas as pd
+import requests
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+All of the above are the required packages for this project to work. I will 
+use Selenium as the websearch engine and pandas as the file manager. 
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# This class will manage everything related to the files (reading, creating and editing)
+class FileManager:
+
+    def __init__(self):
+        self.keywords = None
+        self.websites = None
+
+    def read_files(self):
+        self.websites = pd.read_csv('websites.csv').stack().explode()
+        self.keywords = pd.read_csv('keywords.csv').stack().explode()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# This class will manage the web search based on the websites and keywords files
+class WebSearch:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self):
+        self.status = None
+        self.req = None
+        self.session = None
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/99.0.4844.74 Safari/537.36 ',
+        }
+        self.driver = None
+        self.service = None
+        self.count = 0
+
+    def open_website(self, website_list, keywords_list):
+
+        self.session = requests.session()
+
+        # self.service = ChromeService(executable_path=ChromeDriverManager().install())
+        # self.driver = webdriver.Chrome(service=self.service)
+
+        # path = 'chromedriver.exe'
+        # self.driver = webdriver.Chrome(path)
+
+        for website in website_list[:100]:
+
+            # self.driver.get('https://' + website)
+
+            try:
+
+                self.req = self.session.get('https://' + website + '/', headers=self.headers, verify=False)
+                self.status = self.req.status_code
+                self.count += 1
+
+                time.sleep(3)
+
+                print('https://' + website + '/')
+                print(self.status)
+                print(self.count)
+
+            except:
+
+                self.req = self.session.get('http://' + website + '/', headers=self.headers, verify=False)
+                self.status = self.req.status_code
+                self.count += 1
+
+                time.sleep(3)
+
+                print('http://' + website + '/')
+                print(self.status)
+                print(self.count)
+
+        self.driver
+
+
+x = FileManager()
+
+x.read_files()
+
+y = WebSearch()
+
+y.open_website(x.websites, x.keywords)
+
+y
