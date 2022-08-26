bc810fc9d09188c72a70f06d2af14dd3759cbff1
Nicolas Valencia <n_valencia@hotmail.es>
2022-08-26 08:44:56 -0500

    Final script before final test with all requirements met.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import time
 import pandas as pd
 import requests
+import re
 
 """
 All of the above are the required packages for this project to work. I will 
-use Selenium as the websearch engine and pandas as the file manager. 
+use Selenium as the websearch engine, pandas as the file manager and Regex for the keyword lookup. 
 """
+
+# This dictionary will store the results from each website, to later be saved on a CSV file.
+output = {
+    "Website": [],
+    "Results": []
+}
 
 
 # This class will manage everything related to the files (reading, creating and editing)
@@ -15,14 +22,21 @@ class FileManager:
         self.keywords = None
         self.websites = None
 
+    # Read both "websites" and "keywords" CSV files
     def read_files(self):
         self.websites = pd.read_csv('websites.csv').stack().explode()
         self.keywords = pd.read_csv('keywords.csv').stack().explode()
 
+    # Saves results in both CSV and XLSX files
+    def ouput_file(self, output_dict):
+        pd.DataFrame(output_dict).to_excel('Output.xlsx', header=True, index=False)
+        pd.DataFrame(output_dict).to_csv('Output.csv', header=True, index=False)
 
-# This class will manage the web search based on the websites and keywords files
+
+# This class will manage the web search based on the websites and keywords CSV files
 class WebSearch:
 
+    # Naming all the variables we'll need for the scraper to work
     def __init__(self):
         self.status = None
         self.req = None
@@ -35,19 +49,16 @@ class WebSearch:
         self.service = None
         self.count = 0
 
-    def open_website(self, website_list, keywords_list):
+    """will go through all the websites and search for each keyword on the "Keywords" File. If the page is not 
+    secure it will search it with "http" instead of "https", if it doesn't get resultes like this it will output
+    a "Not found" on the results."""
+
+    def open_website(self, website_list, keywords_list, output_dict):
 
         self.session = requests.session()
 
-        # self.service = ChromeService(executable_path=ChromeDriverManager().install())
-        # self.driver = webdriver.Chrome(service=self.service)
-
-        # path = 'chromedriver.exe'
-        # self.driver = webdriver.Chrome(path)
-
-        for website in website_list[:100]:
-
-            # self.driver.get('https://' + website)
+        for website in website_list:
+            output_dict['Website'].append(website)
 
             try:
 
@@ -55,33 +66,59 @@ class WebSearch:
                 self.status = self.req.status_code
                 self.count += 1
 
+                # Default time to wait between each page, the number is the amount of seconds between each search.
                 time.sleep(3)
 
-                print('https://' + website + '/')
-                print(self.status)
-                print(self.count)
+                found_word = 'N'
 
+                for word in keywords_list:
+
+                    search = re.search(word, self.req.text)
+
+                    if search is not None:
+                        found_word = 'Y'
+
+                    else:
+                        pass
+
+                output_dict['Results'].append(found_word)
+
+            # This starts the run again for the website with http instead of https
             except:
 
-                self.req = self.session.get('http://' + website + '/', headers=self.headers, verify=False)
-                self.status = self.req.status_code
-                self.count += 1
+                try:
+                    self.req = self.session.get('http://' + website + '/', headers=self.headers, verify=False)
+                    self.status = self.req.status_code
+                    self.count += 1
 
-                time.sleep(3)
+                    time.sleep(3)
 
-                print('http://' + website + '/')
-                print(self.status)
-                print(self.count)
+                    found_word = 'N'
 
-        self.driver
+                    for word in keywords_list:
+
+                        search = re.search(word, self.req.text)
+
+                        if search is not None:
+                            found_word = 'Y'
+
+                        else:
+                            pass
+
+                    output_dict['Results'].append(found_word)
+
+                # Page doesn't exist, this is the output
+                except:
+                    output_dict['Results'].append('Not Found')
 
 
-x = FileManager()
+# This last part runs the entire script.
+file_manager = FileManager()
 
-x.read_files()
+file_manager.read_files()
 
-y = WebSearch()
+web_searcher = WebSearch()
 
-y.open_website(x.websites, x.keywords)
+web_searcher.open_website(file_manager.websites, file_manager.keywords, output)
 
-y
+file_manager.ouput_file(output)
